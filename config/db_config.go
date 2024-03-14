dff92049894afd12eead9e28dd55808ad8f59499
Carlos <csanchez@tribalworldwide.gt>
2024-03-14 06:31:19 -0600

    Update app.yaml and db_config.go

--- config/db_config.go
+++ config/db_config.go
@@ -9,7 +9,7 @@ import (
 
 func SetupDB() *sql.DB {
 
-	db, err := sql.Open("mysql", "root:1234@tcp(34.174.184.45:3306)/prueba_yofio")
+	db, err := sql.Open("mysql", "admin:12345678@tcp(database-1.cnu88ou2mbsi.us-east-2.rds.amazonaws.com:3306)/prueba_yofio")
 
 	if err != nil {
 		fmt.Printf("\nError al conectar a la base de datos: %v\n", err)
